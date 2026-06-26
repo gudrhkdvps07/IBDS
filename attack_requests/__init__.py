a51f400cd2805ed192a008c2d49a7d17558f4cfb
gnee1004 <geunhee1459@naver.com>
2026-06-26 16:32:07 +0900

    파일명 수정

--- attack_requests/__init__.py
+++ attack_requests/__init__.py
@@ -4,6 +4,6 @@ This package defines attack request patterns only.
 It does not mutate real requests, send HTTP traffic, or decide findings.
 """
 
-from .request_list import build_attack_request_list, export_attack_request_list
+from .requests_list import build_attack_request_list, export_attack_request_list
 
 __all__ = ["build_attack_request_list", "export_attack_request_list"]
